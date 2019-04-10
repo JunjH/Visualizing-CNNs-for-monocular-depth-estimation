8a22be0dd2297e3363674d47e993f9c455da049c
JunjH <40188741+JunjH@users.noreply.github.com>
2019-04-10 15:32:43 +0900

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -21,33 +21,3 @@ class Sobel(nn.Module):
         out = out.contiguous().view(-1, 2, x.size(2), x.size(3))
   
         return out
-
-class TV(nn.Module):
-    def __init__(self):
-        super(TV, self).__init__()
-        self.conv = nn.Conv2d(1, 1, kernel_size=2, stride=1, bias=False)
-        self.conv2 = nn.Conv2d(1, 1, kernel_size=2, stride=1, bias=False)
-        tv_kx = torch.from_numpy(np.array([[-1, 1],[0, 0]])).float().view(2, 2)
-        tv_ky = torch.from_numpy(np.array([[-1, 0],[1, 0]])).float().view(2, 2)
-
-        tv_kx.unsqueeze_(0)
-        tv_kx.unsqueeze_(0)
-
-        tv_ky.unsqueeze_(0)
-        tv_ky.unsqueeze_(0)
-
-
-        self.conv.weight = nn.Parameter(tv_kx.expand_as(self.conv.weight))
-        self.conv2.weight = nn.Parameter(tv_ky.expand_as(self.conv2.weight))
-        
-        for param in self.parameters():
-            param.requires_grad = False
-
-    def forward(self, x):
-        tv_x = self.conv(x).contiguous()
-        tv_y = self.conv2(x).contiguous()
-
-        # tv = torch.sqrt(torch.pow(torch.abs(tv_x),2) + torch.pow(torch.abs(tv_y),2)).mean()
-        tv = (torch.abs(tv_x) + torch.abs(tv_y)).mean()
-
-        return tv
